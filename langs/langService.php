880cb2b990a0164323039a2d334451af086acfcb
William Pereira <pereirawe@gmail.com>
2021-02-12 11:43:25 -0300

    More translate

---
+++ langs/langService.php
@@ -0,0 +1,15 @@
+<?php
+
+$acceptLang = ['es', 'pt'];
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
+if (isset($_GET['lang'])) {
+    $lang = $_GET['lang'];
+};
+
+$lang = in_array($lang, $acceptLang) ? $lang : 'pt';
+
+require_once "./langs/" . $lang . ".php";
+$langContent = 'content_' . $lang;
+$langURLGet = "?lang=" . $lang;
+$content = new $langContent();
